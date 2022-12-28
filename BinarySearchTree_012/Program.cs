aa92b96deec69499723cad8d7f6d4e79442b4764
Yogaprstyae <m.yoga.ft21@mail.umy.ac.id>
2022-12-28 17:12:10 +0700

    add method node

--- BinarySearchTree_012/Program.cs
+++ BinarySearchTree_012/Program.cs
@@ -9,10 +9,16 @@ namespace BinarySearchTree_012
     class Node
     {
         public string info;
-        public string leftchild;
-        public string rightchild;
+        public Node leftchild;
+        public Node rightchild;
 
         // Constructor for the node class
+        public Node(string i, Node l, Node r)
+        {
+            info = i;
+            leftchild = l;
+            rightchild = r;
+        }
         static void Main(string[] args)
         {
         }
